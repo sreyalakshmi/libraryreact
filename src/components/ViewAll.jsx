65e9baeeee2931346cbb196ed8117f45ad8d8546
sreyalakshmi <sreyalakshmisaji@gmail.com>
2024-06-07 13:37:27 +0530

    viewall complete

---
+++ src/components/ViewAll.jsx
@@ -0,0 +1,76 @@
+import React from 'react'
+
+const ViewAll = () => {
+  return (
+    <div>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">title</th>
+      <th scope="col">author</th>
+      <th scope="col">published year</th>
+      <th scope="col">price</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td scope="row">harry potter</td>
+      <td>jk rowling</td>
+      <td>1987</td>
+      <td>$200</td>
+    </tr>
+    <tr>
+    <td scope="row">heiddi</td>
+      <td>jk</td>
+      <td>1987</td>
+      <td>$100</td>
+    </tr>
+    <tr>
+    <td scope="row">hellen keller</td>
+      <td>hellen</td>
+      <td>1987</td>
+      <td>$150</td>
+    </tr>
+    <tr>
+    <td scope="row">wings of fire</td>
+      <td>apj</td>
+      <td>1990</td>
+      <td>$150</td>
+    </tr>
+    <tr>
+    <td scope="row">melencoly</td>
+      <td>heen</td>
+      <td>1987</td>
+      <td>$150</td>
+    </tr>
+    <tr>
+    <td scope="row">2 states</td>
+      <td>chetan bagat</td>
+      <td>2019</td>
+      <td>$150</td>
+    </tr>
+    <tr>
+    <td scope="row">half girlfiend</td>
+      <td>chetan bagat</td>
+      <td>2026</td>
+      <td>$150</td>
+    </tr>
+    <tr>
+    <td scope="row">ikigai</td>
+      <td>susoki</td>
+      <td>2020</td>
+      <td>$200</td>
+    </tr>
+  </tbody>
+</table>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ViewAll
